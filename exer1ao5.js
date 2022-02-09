e1070cbc249a08d3aa0985118d5437610ff966f7
Rafael <rafaeldovale666@gmail.com>
2022-02-09 20:47:05 -0300

    realizado o terceiro exercício

--- exer1ao5.js
+++ exer1ao5.js
@@ -11,7 +11,21 @@ Ex:
 Qual é o seu nome? João da Silva
 Olá João da Silva, é um prazer te conhecer!*/
 
-var nome = prompt("qual é seu nome?")
+/*var nome = prompt("qual é seu nome?")
 
-console.log("Olá, " +nome +" é um prazer te conhecer!")
+alert("Olá, " +nome +" é um prazer te conhecer!")*/
 
+
+
+/*3) Crie um programa que leia o nome e o salário de um funcionário, mostrando no
+final uma mensagem.
+Ex:
+Nome do Funcionário: Maria do Carmo
+Salário: 1850,45
+O funcionário Maria do Carmo tem um salário de R$1850,45 em Junho.*/
+
+var nome = prompt("Qual é seu nome?")
+
+var salario = prompt("qual é seu salário?")
+
+alert("O funcionário "+ nome+ " tem um salário de R$"+salario+" em Junho" )
