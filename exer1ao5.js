7927645147cad01abb5b63b4cde092b803633d03
Rafael <rafaeldovale666@gmail.com>
2022-02-09 20:26:59 -0300

    Resolvido segundo exercícios

--- exer1ao5.js
+++ exer1ao5.js
@@ -4,3 +4,14 @@ console.log("Olá, mundo!")
 
 let frase = 'Olá, mundo!'
 console.log(frase)
+
+
+/*2) Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boasvindas para ela:
+Ex:
+Qual é o seu nome? João da Silva
+Olá João da Silva, é um prazer te conhecer!*/
+
+var nome = prompt("qual é seu nome?")
+
+console.log("Olá, " +nome +" é um prazer te conhecer!")
+
