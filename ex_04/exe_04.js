8e784c6653def68152db0d5fe37e8392eeb5cc69
Rafael <rafaeldovale666@gmail.com>
2022-02-14 14:11:36 -0300

    resolvendo ex. 4

---
+++ ex_04/exe_04.js
@@ -0,0 +1,17 @@
+/*4) Desenvolva um algoritmo que leia dois números inteiros e mostre o somatório
+entre eles.
+Ex:
+Digite um valor: 8
+Digite outro valor: 5
+A soma entre 8 e 5 é igual a 13.*/
+
+var a = prompt("Digite um valor:")
+a = parseFloat(a);
+
+var b = prompt("Digite outro valor:")
+b = parseFloat(b)
+
+var result = a+b
+
+alert(" A soma entre "+ a + " e " + b + " é igual a "+ result)
+
