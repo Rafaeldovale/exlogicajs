151d78e0d75179961c947296cd873acbe0ab49ea
Rafael <rafaeldovale666@gmail.com>
2022-02-14 13:41:13 -0300

    revisando exercícios

---
+++ ex_03/exe_03.js
@@ -0,0 +1,12 @@
+/*3) Crie um programa que leia o nome e o salário de um funcionário, mostrando no
+final uma mensagem.
+Ex:
+Nome do Funcionário: Maria do Carmo
+Salário: 1850,45
+O funcionário Maria do Carmo tem um salário de R$1850,45 em Junho.*/
+
+var nome = prompt("Qual é seu nome?")
+
+var salario = prompt("qual é seu salário?")
+
+alert("O funcionário "+ nome+ " tem um salário de R$"+salario+" em Junho" )
